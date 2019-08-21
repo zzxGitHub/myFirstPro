26d730f464c6220c0ec93a4c4b309e3daa42776e
zhangzexiong <837786367@qq.com>
2019-08-21 09:34:35 +0800

    提交记录2

--- src/main/java/com/example/demo/service/impl/ElasticSearchServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ElasticSearchServiceImpl.java
@@ -100,7 +100,8 @@ public class ElasticSearchServiceImpl implements ElasticSearchService {
 	}
 	
 	public static void main(String[] args) {
-		
+		System.out.println("111111111111111");
+		System.out.println("111111111111111");
 	}
 
 }
